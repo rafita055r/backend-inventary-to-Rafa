2ceb8b5168abe880b1c2b67363910e0e626388c5
EzeGomez0407 <gomezmerloe@gmail.com>
2026-02-04 08:52:59 -0300

    probando servidor

--- services/getTools.js
+++ services/getTools.js
@@ -1,10 +1,8 @@
-const supabase = require("../supabaseClient")
+const supabase = require("../supabaseClient");
 
-async function getTools(nameTool){
-    try {
-        const {error, data} = await supabase
-            .from("herramientas")
-            .select(`
+async function getTools(nameTool) {
+  try {
+    const { error, data } = await supabase.from("herramientas").select(`
                 *,
                 herramientas_en_obras!herramientas_en_obras_herramienta_id_fkey (
                     id,
@@ -16,47 +14,50 @@ async function getTools(nameTool){
                 )
             `);
 
-        if (error) throw error
+    if (error) throw error;
 
-        const toolsListFormated = data.map(tool=>{
-            return {
-                id: tool.id,
-                nombre: tool.nombre,
-                marca: tool.marca,
-                estado: tool.estado,
-                cantidad_total: tool.cantidad_total,
-                medidas: tool.medidas,
-                observacion: tool.observacion,
-                ubications: tool.herramientas_en_obras.map(register => ({
-                    register_id: register.id,
-                    work_current_id: register.obras.id,
-                    work_name: register.obras.nombre,
-                    quantityOnWork: register.cantidad
-                }))
+    const toolsListFormated = data.map((tool) => {
+      return {
+        id: tool.id,
+        nombre: tool.nombre,
+        marca: tool.marca,
+        estado: tool.estado,
+        cantidad_total: tool.cantidad_total,
+        medidas: tool.medidas,
+        observacion: tool.observacion,
+        ubications: tool.herramientas_en_obras.map((register) => ({
+          register_id: register.id,
+          work_current_id: register.obras.id,
+          work_name: register.obras.nombre,
+          quantityOnWork: register.cantidad,
+        })),
+      };
+    });
 
-            }
-        })
+    if (!nameTool || nameTool.trim() === "") {
+      return { data: toolsListFormated, error: null };
+    } else {
+      const searchedResult = toolsListFormated.filter((tool) =>
+        String(tool.nombre)
+          .trim()
+          .toLowerCase()
+          .includes(String(nameTool).trim().toLowerCase())
+      );
 
-        if(!nameTool || nameTool.trim() === ""){
-            return {data: toolsListFormated, error: null};
-        }else{
-            const searchedResult = toolsListFormated.filter((tool) => String(tool.nombre).trim().toLowerCase().includes(String(nameTool).trim().toLowerCase()))
-
-            return {data: searchedResult, error: null}
-        }
-        
-        
-    } catch (error) {
-        console.log(error);
-        return {data: null, error}
+      return { data: searchedResult, error: null };
     }
+  } catch (error) {
+    console.log(error);
+    return { data: null, error };
+  }
 }
 
 async function getToolByID(tool_id) {
-    try {
-        const {error, data} = await supabase
-            .from("herramientas")
-            .select(`
+  try {
+    const { error, data } = await supabase
+      .from("herramientas")
+      .select(
+        `
                 *,
                 herramientas_en_obras!herramientas_en_obras_herramienta_id_fkey (
                     id,
@@ -66,36 +67,36 @@ async function getToolByID(tool_id) {
                         nombre
                     )
                 )
-            `)
-            .eq("id", tool_id)
-            .single();
-        
-        if(error) throw error        
+            `
+      )
+      .eq("id", tool_id)
+      .single();
 
-        const tool = {
-            id: data.id,
-            nombre: data.nombre,
-            marca: data.marca,
-            estado: data.estado,
-            cantidad_total: data.cantidad_total,
-            medidas: data.medidas,
-            observacion: data.observacion,
-            ubications: data.herramientas_en_obras.map(register => ({
-                register_id: register.id,
-                work_current_id: register.obras.id,
-                work_name: register.obras.nombre,
-                quantityOnWork: register.cantidad
-            }))
+    if (error) throw error;
 
-        }
-           return {data: tool, error}
-    } catch (error) {
-        console.log(error);
-        return {error}
-    }
+    const tool = {
+      id: data.id,
+      nombre: data.nombre,
+      marca: data.marca,
+      estado: data.estado,
+      cantidad_total: data.cantidad_total,
+      medidas: data.medidas,
+      observacion: data.observacion,
+      ubications: data.herramientas_en_obras.map((register) => ({
+        register_id: register.id,
+        work_current_id: register.obras.id,
+        work_name: register.obras.nombre,
+        quantityOnWork: register.cantidad,
+      })),
+    };
+    return { data: tool, error };
+  } catch (error) {
+    console.log(error);
+    return { error };
+  }
 }
 
 module.exports = {
-    getTools,
-    getToolByID
-}
+  getTools,
+  getToolByID,
+};
